904d364e9f09864844ddede873e2a9b2d337c166
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:01:10 -0400

    added variables

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const { printTable } = require("console-table-printer");
 const figlet = require("figlet");
+
+// Variables
+let roles;
+let departments;
+let managers;
+let employees;
