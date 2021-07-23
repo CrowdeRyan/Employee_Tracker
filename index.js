7310c32a91b2e9d08f9c32e3e2d0c1e4a2c7ba9e
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 14:01:35 -0400

    added secure login

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const { printTable } = require("console-table-printer");
 const figlet = require("figlet");
 
 // Secure connection
-const sequelize = require("connection");
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 // Variables
 let roles;
