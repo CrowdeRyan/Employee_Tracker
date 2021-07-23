d1c635f4ac55bbd0df9f076747f418a711be8e88
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:17:50 -0400

    employee adding function, forgot support functions

--- index.js
+++ index.js
@@ -209,6 +209,8 @@ addRole = () => {
 
 // Employee section of adding menu
 addEmployee = () => {
+  getRoles();
+  getManagers();
   let roleOptions = [];
   for (i = 0; i < roles.length; i++) {
     roleOptions.push(Object(roles[i]));
