c6b9edb8d3f060189678b1bc931a83d689f8933f
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:06:58 -0400

    secured connection

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ const inquirer = require("inquirer");
 const { printTable } = require("console-table-printer");
 const figlet = require("figlet");
 
+// Secure connection
+const sequelize = require("connection");
+
 // Variables
 let roles;
 let departments;
 let managers;
 let employees;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: process.env.DB_USER,
+
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: "employees_db",
+});
