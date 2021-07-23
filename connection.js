33072d2e51c59e8450419d834038e65a1b4b7933
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:58:23 -0400

    integrated into index

--- connection.js
+++ connection.js
@@ -1,15 +1,17 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+// *** Integrated into Index.js ***
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
 
-module.exports = sequelize;
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
+
+// module.exports = sequelize;
