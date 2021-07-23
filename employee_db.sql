4b301f078cba91bf1c8c631dea9a227df4f5b7ad
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 12:55:55 -0400

    added queries

--- employee_db.sql
+++ employee_db.sql
@@ -28,3 +28,12 @@ CREATE TABLE employee (
   manager_id INT, 
   PRIMARY KEY (id)
 );
+
+-- Query to view all --
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
+-- Query to view all roles --
+SELECT  r.id, r.title, r.salary, d.name as Department_Name FROM role AS r INNER JOIN department AS d ON r.department_id = d.id;
+
+--Query to get employees --
+SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee
