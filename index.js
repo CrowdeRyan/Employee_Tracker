e89903a3044d75b598c2c46c43701e667ca54697
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:08:49 -0400

    beginning function

--- index.js
+++ index.js
@@ -36,3 +36,33 @@ connection.connect(function (err) {
   getManagers();
   getEmployees();
 });
+
+// Intiating prompts
+start = () => {
+  inquirer
+    .prompt({
+      name: "choices",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"],
+    })
+    .then(function (answer) {
+      if (answer.choices === "ADD") {
+        addSomething();
+      } else if (answer.choices === "VIEW") {
+        viewSomething();
+      } else if (answer.choices === "UPDATE") {
+        updateSomething();
+      } else if (answer.choices === "DELETE") {
+        deleteSomething();
+      } else if (answer.choices === "EXIT") {
+        figlet("Thanks for using FSC Employee Tracker", (err, result) => {
+          console.log(err || result);
+        });
+
+        connection.end();
+      } else {
+        connection.end();
+      }
+    });
+};
