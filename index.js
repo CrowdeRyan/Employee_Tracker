a16b4eb4f939d13de94a2518eb2f9dcf69e2ea6c
Ryan Crowder <crowderyan@gmail.com>
2021-07-23 13:12:27 -0400

    employee select function

--- index.js
+++ index.js
@@ -93,3 +93,15 @@ getManagers = () => {
     }
   );
 };
+
+// Employee Selection
+getEmployees = () => {
+  connection.query(
+    "SELECT id, CONCAT_WS(' ', first_name, last_name) AS Employee_Name FROM employee",
+    (err, res) => {
+      if (err) throw err;
+      employees = res;
+      // console.table(employees);
+    }
+  );
+};
